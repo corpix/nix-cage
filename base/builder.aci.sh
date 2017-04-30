dd55711f57fc6d3ca9c5ff835d3795812791c54a
Dmitry Moskowski <me@corpix.ru>
2017-04-30 19:27:41 +0000

    Create non privileged user

--- base/builder.aci.sh
+++ base/builder.aci.sh
@@ -6,4 +6,9 @@ ac run -- sh -c "
    $(proxy)
    dnf group install -y --best 'development tools'
    dnf install -y --best emacs make tmux tree mc htop iotop zsh unzip bsdtar
+   useradd -md /home/user -U user
+   usermod -p '*' user
 "
+
+ac set-user user
+ac set-working-directory /home/user
