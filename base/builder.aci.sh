76f4a3c30a8bbd39ac8ce2504b3b4a6552cf5f30
Dmitry Moskowski <me@corpix.ru>
2017-05-22 14:14:06 +0000

    Fixing locale

--- base/builder.aci.sh
+++ base/builder.aci.sh
@@ -13,13 +13,15 @@ ac run -- sh -c "
        unzip bsdtar sudo xz tar p7zip                \
        openssh-clients openssh-server                \
        bind-utils iputils iproute nmap git           \
-       aspell aspell-en                              \
+       aspell aspell-en glibc-locale-source          \
        'dnf-command(copr)'
    mkdir /etc/emacs
    dnf clean all
    rm -rf /tmp/*
    echo 'nameserver 208.67.222.222' >  /etc/resolv.conf
    echo 'nameserver 208.67.220.220' >> /etc/resolv.conf
+   echo 'LANG=\"en_US.UTF-8\"'      >  /etc/locale.conf
+   localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
 "
 
 ac set-working-directory /home/user
@@ -27,6 +29,7 @@ ac set-working-directory /home/user
 ac mount add projects     /home/user/Projects
 ac mount add emacs        /home/user/.emacs.d
 
+ac environment add LANG       en_US.UTF-8
 ac environment add PATH       /home/user/Projects/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 ac environment add TERM       screen-256color
 ac environment add DEMOTE_UID 1000
