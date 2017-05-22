f6c79fca60cad899c2567b59098f93cdb6c9dade
Dmitry Moskowski <me@corpix.ru>
2017-05-22 14:23:20 +0000

    Do not fail if localedef fails

--- base/builder.aci.sh
+++ base/builder.aci.sh
@@ -21,7 +21,7 @@ ac run -- sh -c "
    echo 'nameserver 208.67.222.222' >  /etc/resolv.conf
    echo 'nameserver 208.67.220.220' >> /etc/resolv.conf
    echo 'LANG=\"en_US.UTF-8\"'      >  /etc/locale.conf
-   localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
+   localedef -v -c -i en_US -f UTF-8 en_US.UTF-8 || true
 "
 
 ac set-working-directory /home/user
