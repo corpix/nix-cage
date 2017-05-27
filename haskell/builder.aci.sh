78e206674a5a1506995ef7c81dc9134425867c50
Dmitry Moskowski <me@corpix.ru>
2017-05-27 01:13:41 +0000

    Added local profix to PATH for haskell

--- haskell/builder.aci.sh
+++ haskell/builder.aci.sh
@@ -8,6 +8,8 @@ ac run -- sh -c "
    dnf copr enable -y petersen/stack
    dnf install -y --best ghc ghc-devel cabal-install stack
    stack upgrade
+   echo 'export PATH=/home/user/.local/bin:\$PATH' >> /etc/profile.d/path.sh
+   chmod +x /etc/profile.d/path.sh
    dnf clean all
    rm -rf /tmp/*
 "
