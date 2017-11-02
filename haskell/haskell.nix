f753c4ceda19f8eae6b7f6162432b8cf7cddc372
Dmitry Moskowski <me@corpix.ru>
2017-11-02 12:41:07 +0000

    Added more haskell tools

--- haskell/haskell.nix
+++ haskell/haskell.nix
@@ -3,5 +3,10 @@
 {
   environment.systemPackages = with pkgs; [
     stack
+    haskellPackages.hlint
+    haskellPackages.hindent
+    haskellPackages.stylish-haskell
+    haskellPackages.cabal-install
+    haskellPackages.intero
   ];
 }
