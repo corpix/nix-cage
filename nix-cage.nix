109af96846bfed118fe317295f2f3413e36d2d40
Dmitry Moskowski <me@corpix.ru>
2018-02-06 01:29:30 +0000

    Fixed nix derivation dependencies

--- nix-cage.nix
+++ nix-cage.nix
@@ -1,24 +1,30 @@
 with import <nixpkgs> {};
-python36Packages.buildPythonApplication {
+stdenv.mkDerivation rec {
 
   name = "nix-cage";
 
-  buildInputs = [ bubblewrap ];
+  nativeBuildInputs = [ makeWrapper ];
 
   src = fetchFromGitHub {
     owner  = "corpix";
-    repo   = "nix-cage";
+    repo   = name;
     rev    = "a731efaabcb67a4e9db933f452b7681229d64baa";
     sha256 = "1fn4wx6pjmy899qjg7nq0sm66ky3ll69mdica0h7577sgsldm9cm";
   };
 
-  builder = builtins.toFile "builder.sh" ''
+  installPhase = ''
     source $stdenv/setup
     set -e
 
     mkdir -p $out/bin
-    cp       $src/nix-cage    $out/bin
-    chmod +x $out/bin/nix-cage
+    cp       $src/${name}    $out/bin
+    chmod +x $out/bin/${name}
+
+    wrapProgram $out/bin/${name} --prefix PATH : ${stdenv.lib.makeBinPath [
+      python36
+      bubblewrap
+      nix
+    ]}
   '';
 
   meta = {
