9efac72ad0095ae6e07fcd0d3f50666b8cef84e6
Dmitry Moskowski <me@corpix.ru>
2019-04-18 17:00:27 +0000

    more complete and concrete shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,15 @@
-with import <nixpkgs> {};
+let
+  nixpkgs = builtins.fetchTarball {
+    url    = "https://github.com/nixos/nixpkgs/archive/03050e9749e1548f1648aae5c062c954eaad546e.tar.gz";
+    sha256 = "00hfkldmf853ynnd8a9d7a778ifcrdjxdndxyykzbpxfki5s5qsb";
+  };
+in with import nixpkgs {};
 stdenv.mkDerivation {
-  name = "nix-cage-shell";
+  name = "nix-shell";
   buildInputs = [
-    python36
+    nix
+    python3
+    git
+    gnumake
   ];
 }
