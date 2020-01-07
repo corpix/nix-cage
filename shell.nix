7023988c92f066f9f345b7383e5de2163ca67e31
Dmitry Moskowski <me@corpix.ru>
2020-01-07 04:29:15 +0000

    shell: fixing nixpkgs to specific commit

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-with import <nixpkgs> {}; stdenv.mkDerivation {
+let
+  nixpkgs = builtins.fetchTarball {
+    url    = "https://github.com/nixos/nixpkgs/archive/7eaaf728e4f1e37637d20204e7102db7ccf3b10c.tar.gz";
+    sha256 = "0k02qw9ivb8y5whp4xysnwyp3rw1lqrb8khaazf43zpksmrncw2j";
+  };
+in with (import nixpkgs {}); stdenv.mkDerivation {
   name = "nix-shell";
   buildInputs = [
     gnumake
@@ -6,4 +11,7 @@ with import <nixpkgs> {}; stdenv.mkDerivation {
     python3
     jq
   ];
+  shellHook = ''
+    export NIX_PATH=nixpkgs=${nixpkgs}
+  '';
 }
